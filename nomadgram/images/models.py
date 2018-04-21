b4988553a99dfb3564de3b8d8a0d04fb1b8b3455
Kunho Lee <gunho1020@gmail.com>
2018-04-21 10:39:28 +0900

    2018-04-21,Sat,10:39

---
+++ nomadgram/images/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from nomadgram.users import models as user_models
+
+# Create your models here.
+
+class TimeStampeModel(models.Model):
+
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+
+  class Meta:
+    abstract = True
+
+class Image(TimeStampeModel):
+
+  """ Image Model """
+
+  file = models.ImageField()
+  location = models.CharField(max_length = 140)
+  caption = models.TextField()
+  creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
+
+
+class Comment(TimeStampeModel):
+
+  """ Comment Model """
+
+  message = models.TextField()
+  creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
+  image = models.ForeignKey(Image, null = True, on_delete = models.CASCADE)
+
+class Like(TimeStampeModel):
+
+  """ Like Model """
+
+  creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
+  image = models.ForeignKey(Image, null = True, on_delete = models.CASCADE)
